acb3f8508471e24eedd16af67de8acdf9f51a562
zainabAlhaddad <76396982+zainabAlhaddad@users.noreply.github.com>
2020-12-23 20:13:47 +0300

    Exercise1&2

---
+++ Exercise1/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int level;
+    string CurrentScreen = "Main Menu";
+
+    void Start()
+    {
+        showMainMenu("let's Start");
+    }
+
+    void showMainMenu(string greeting)
+    {
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Press 1 for Firs level");
+        Terminal.WriteLine("Press 2 for Second level");
+    }
+
+    void setLevel( String selectedLevel)
+    {
+        if (selectedLevel == "1")
+        {
+            level = 1;
+        }
+        if else (selectedLevel == "2"){
+            level = 2;
+
+        }
+
+    }
+
+    void OnUsereInput(string input)
+    {
+        if (CurrentScreen=="Main Menu ")
+        {
+            setLevel(input);
+        }
+
+
+    }
+}
